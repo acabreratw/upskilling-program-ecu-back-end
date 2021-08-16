f8f6c289447f9c25b1a6cb9347b35166b00db57d
Jhosep <jhosep.marin@thoughtworks.com>
2021-08-16 15:22:45 -0500

    Applies suggestions made in PR #2

--- src/main/java/com/thoughtworks/lpe/be_template/services/UserDetailsServiceImpl.java
+++ src/main/java/com/thoughtworks/lpe/be_template/services/UserDetailsServiceImpl.java
@@ -2,6 +2,7 @@ package com.thoughtworks.lpe.be_template.services;
 
 import com.thoughtworks.lpe.be_template.domains.ApplicationUser;
 import com.thoughtworks.lpe.be_template.repositories.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,11 +13,8 @@ import static java.util.Collections.emptyList;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
     private ApplicationUserRepository applicationUserRepository;
-
-    public UserDetailsServiceImpl(ApplicationUserRepository applicationUserRepository) {
-        this.applicationUserRepository = applicationUserRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
