4a38a8512f01a4208c18d0d53c96bcefceafb7aa
Jhosep <jhosep.marin@thoughtworks.com>
2021-08-19 18:37:52 -0500

    Adds token decryption method and yaml property to get custom token key

--- src/main/java/com/thoughtworks/lpe/be_template/security/TokenDecoder.java
+++ src/main/java/com/thoughtworks/lpe/be_template/security/TokenDecoder.java
@@ -1,16 +1,30 @@
 package com.thoughtworks.lpe.be_template.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
+import java.util.Map;
 
 @Component
 public class TokenDecoder {
+    @Value("${spring.auth0.custom-token-key}")
+    private String customTokenKey;
+
     public String getTokenPayload(String accessToken) {
         String[] chunks = accessToken.split("\\.");
 
         Base64.Decoder decoder = Base64.getDecoder();
 
         return new String(decoder.decode(chunks[1]));
+    }
+
+    public String getCustomPropertyFromToken(String tokenPayload, String customPropertyToGet) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> object = objectMapper.readValue(tokenPayload, Map.class);
+
+        return object.get(customTokenKey+ "/" +customPropertyToGet);
     }
 }
