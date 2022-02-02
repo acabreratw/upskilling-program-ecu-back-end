b0aacc4d741faf962f8398265e1d42f0fb863379
Israel Ona <israel.ona@thoughtworks.com>
2022-02-02 17:02:17 -0500

    fix migrations

---
+++ migrations/db/migrations/4-insert-records-into-category-table.sql
@@ -0,0 +1,17 @@
+INSERT INTO category (category_name)
+VALUES  ('Technology');
+
+INSERT INTO category (category_name)
+VALUES  ('Science');
+
+INSERT INTO category (category_name)
+VALUES  ('Business');
+
+INSERT INTO category (category_name)
+VALUES  ('Marketing');
+
+INSERT INTO category (category_name)
+VALUES  ('Personal Development');
+
+INSERT INTO category (category_name)
+VALUES  ('Creativity');
---
+++ migrations/db/migrations/5-insert-records-into-trainer-table.sql
@@ -0,0 +1,8 @@
+INSERT INTO trainer (`degree`, description, name)
+VALUES  ('degree test', 'test', 'Luis Zarate');
+
+INSERT INTO trainer (`degree`, description, name)
+VALUES  ('degree test', 'test', 'Armando Parra');
+
+INSERT INTO trainer (`degree`, description, name)
+VALUES  ('degree test', 'test', 'Sofía Perez');
